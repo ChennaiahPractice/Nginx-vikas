c353c2c7ba6b14bf17d72bfede844511f067a232
Your Name <you@example.com>
2017-05-06 19:05:04 +0000

    sample shell file

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ sudo apt-get install nginx -y
 sudo ufw allow 'Nginx HTTP'
 sudo ufw status
 sudo apt-get update
-sudo apt-get install mysql-server
-sudo mysql_secure_installation
-sudo apt-get install php-fpm php-mysql -y
-add_file="/etc/php/7.0/fpm/php.ini"
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install mysql-server -y
+sudo apt-get install php5-fpm php5-mysql
+add_file="/etc/php5/fpm/php.ini"
 if [ -f $add_file ]
 then 
 	echo "hi vikas file is  avalable"
@@ -14,21 +14,22 @@ then
 else
 	echo "file not avalable"
 fi
-sudo systemctl restart php7.0-fpm
+sudo service php5-fpm restart
 defalt_file="/etc/nginx/sites-available/default"
 if [ -f $defalt_file  ]
 then
 	echo "hi vikas doing changes in this file"
 	sed -i '39,47d' $defalt_file
-	sed -i '/root/Nginxvikas/r serverconf.txt' $defalt_file
+	sed -i '/root/Nginx-vikas/r serverconf.txt' $defalt_file
 fi
 sudo nginx -t
 sudo systemctl reload nginx
-cd /var/www/html
+cd /usr/share/nginx/html/
 touch info.php
-php_file="/var/www/html/info.php"
+php_file="/usr/share/nginx/html/info.php"
 if [ -f $php_file ]
 then 
 	echo "php file also avalable "
-	sed -i '/root/Nginxvikas/r phpconf.txt' $php_file
+	sed -i '/root/Nginx-vikas/r phpconf.txt' $php_file
 fi
+
